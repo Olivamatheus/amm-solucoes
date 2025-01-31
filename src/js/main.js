561f5095f03b016afeb26757c8285f58842d872b
Oliva Matheus <math.oliver133@gmail.com>
2025-01-31 09:57:00 -0300

    Desenvolvimento front end para o sitstema da AMM Soluções
    Iª Página (Home Page)
    Versão desktop completa;
    Versão tablet completa;
    Versão mobile em desenvolvimento;

---
+++ src/js/main.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const orders = Array.from(document.querySelectorAll(".order"));
+    const statusFilter = document.getElementById("status");
+    const lojaCheckboxes = document.querySelectorAll("#dropdownLoja .form-check-input");
+
+    // Função para filtrar pedidos por status
+    statusFilter.addEventListener("change", function () {
+        const selectedStatus = statusFilter.value;
+        orders.forEach(order => {
+            const orderStatus = order.querySelector(".order-status").textContent.toLowerCase();
+            if (selectedStatus === "todos" || orderStatus === selectedStatus) {
+                order.style.display = "block";
+            } else {
+                order.style.display = "none";
+            }
+        });
+    });
+
+    // Função para filtrar pedidos por lojas selecionadas
+    lojaCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", function () {
+            const selectedLojas = Array.from(lojaCheckboxes)
+                .filter(cb => cb.checked)
+                .map(cb => cb.value);
+
+            orders.forEach(order => {
+                const orderLoja = order.querySelector(".store-name").textContent.split(": ")[1].toLowerCase();
+                if (selectedLojas.length === 0 || selectedLojas.includes(orderLoja)) {
+                    order.style.display = "block";
+                } else {
+                    order.style.display = "none";
+                }
+            });
+        });
+    });
+
+    // Script para alternar o estado do botão "Abrir Loja"
+    const abrirLojaBtn = document.getElementById('abrirLojaBtn');
+    abrirLojaBtn.addEventListener('click', () => {
+        console.log('Botão clicado!'); // Verifique se isso aparece no console
+        if (abrirLojaBtn.classList.contains('off')) {
+            abrirLojaBtn.classList.remove('off');
+            abrirLojaBtn.classList.add('on');
+            abrirLojaBtn.textContent = 'Abrir Loja';
+        } else {
+            abrirLojaBtn.classList.remove('on');
+            abrirLojaBtn.classList.add('off');
+            abrirLojaBtn.textContent = 'Fechar Loja';
+        }
+    });
+});
